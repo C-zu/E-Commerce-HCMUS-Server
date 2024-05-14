a33a43bf2067d2e704efb4d7c396f9751676033b
Huy Nguyen <gogorun235@gmail.com>
2024-05-14 18:44:52 +0700

    Update conn.mjs

--- db/conn.mjs
+++ db/conn.mjs
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const connectionString = process.env.ATLAS_URI || "";
+const connectionString = "mongodb+srv://nghia:nghia@se-ecommerce.lkwnlpo.mongodb.net/?retryWrites=true&w=majority&appName=SE-Ecommerce" || "";
 
 const client = new MongoClient(connectionString);
 
